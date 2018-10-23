cd8de3c1f1a199057abcb23d087fb6d930cfc4a0
Niki Smit <jijjezin@gmail.com>
2018-10-23 10:34:09 +0200

    Nikis werk van de afgelopen dagen/weken (dagen in weken geen hele weken, ach, wat nuanceer ik, alleen ik lees dit, hoi niki, ik hop dat je een goede week hebt, hoe is je stoelgang?)

---
+++ Assets/PingpongAxis.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PingpongAxis : MonoBehaviour {
+
+    private Transform attacher;
+    public int height = 10;//max height of Box's movement
+    public float zCenter = 0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        attacher = this.transform.Find("PingpongAxis");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.y, zCenter + Mathf.PingPong(Time.time * 2, height) - height / 2f, transform.position.y);//move on y axis only
+                                                                                                                                                  //Box is moving with Mathf.PingPong (http://docs.unity3d.com/Documentation/ScriptReference/Mathf.PingPong.html)
+    }
+}
